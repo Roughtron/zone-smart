9689dd4b9c4e037d9fd67d47fbdf6f7ab45e05c8
Roughtron <roughtron@yandex.ru>
2021-02-24 09:00:54 +0500

    add heading component

---
+++ src/components/Common/Heading.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+    component.heading(
+        :is="tag"
+    )
+        slot
+</template>
+
+<script>
+export default {
+    props: {
+        tag: {
+            type: String,
+            readonly: true,
+            default: 'h1'
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+  .heading
+    font-family: Roboto, sans-serif
+    font-size: 36px
+    color: #152739
+</style>
